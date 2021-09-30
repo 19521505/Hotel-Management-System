6a0a1d04fe8361b4f4ac0a5c5a4cc4739d91c21c
KSB-tqk <linh05602@gmail.com>
2021-09-30 20:46:11 +0700

    base template

---
+++ lib/component/rounded_button.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import '../constrants.dart';
+
+class RoundedButton extends StatelessWidget {
+  final String text;
+  final VoidCallback press;
+  final Color color, textColor;
+  final double height;
+  final bool useDefaultHeight;
+  const RoundedButton({
+    Key? key,
+    required this.text,
+    required this.press(),
+    this.color = kPrimaryColor,
+    required this.textColor,
+    this.height = 0,
+    this.useDefaultHeight = false,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 5),
+      height: useDefaultHeight ? size.height * 0.1 : height,
+      width: size.width * 0.8,
+      padding: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.all(
+          Radius.circular(29.0),
+        ),
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(29),
+        child: Material(
+          child: TextButton(
+            onPressed: press,
+            child: Text(
+              text,
+              style: TextStyle(
+                  fontWeight: FontWeight.bold, fontSize: 20, color: textColor),
+            ),
+            style: TextButton.styleFrom(
+              padding: EdgeInsets.symmetric(vertical: 5, horizontal: 0),
+              backgroundColor: kPrimaryColor,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/views/login/login_view.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:hotel_management_system/component/rounded_button.dart';
+import 'package:hotel_management_system/component/rounded_input_field.dart';
+import 'package:hotel_management_system/component/rounded_password_field.dart';
+import 'package:hotel_management_system/views/login/widgets/login_background.dart';
+import '../../constrants.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  double _height = 56;
+  GlobalKey _globalKey = GlobalKey();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: LoginBackground(
+        child: SingleChildScrollView(
+          physics: NeverScrollableScrollPhysics(),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 30,
+                ),
+                Image.asset("assets/images/hotel_logo_icon.png"),
+                SizedBox(
+                  height: 10,
+                ),
+                RoundedInputField(
+                  hintText: "Your Email",
+                  onChanged: (value) {},
+                  icon: Icons.person,
+                ),
+                RoundedPasswordField(
+                  onChanged: (value) {},
+                ),
+                RoundedButton(
+                  text: "SIGN IN",
+                  height: _height,
+                  press: () {
+                    if (_formKey.currentState!.validate()) {}
+                  },
+                  textColor: Colors.white,
+                ),
+                SizedBox(height: 10),
+                Image.asset("assets/images/bottom_hotel_logo_icon.png"),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      "Don't have an account ? ",
+                      style: TextStyle(color: Colors.black54),
+                    ),
+                    GestureDetector(
+                      onTap: () {},
+                      child: Text(
+                        "Sign Up",
+                        style: TextStyle(
+                            color: kPrimaryColor, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
