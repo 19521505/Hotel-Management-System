2a5134066641f12be74617c323ada46c6d65326b
Nguyễn Thành Hiếu <19521505@gm.uit.edu.vn>
2021-10-17 14:32:15 +0700

    [DEV, UPDATE] -Form request and edit design

--- lib/views/routes/app_routes.dart
+++ lib/views/routes/app_routes.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:hotel_management_system/views/screens/main/kitchen/import_request/form_foods_request.dart';
 import 'package:hotel_management_system/views/screens/main/kitchen/kitchenpage.dart';
+import 'package:hotel_management_system/views/screens/main/kitchen/request_form/dialog_details_req.dart';
+import 'package:hotel_management_system/views/screens/main/kitchen/request_form/form_foods_request.dart';
 import 'package:hotel_management_system/views/screens/main/main_screen.dart';
 import 'package:hotel_management_system/views/screens/landing/landing_page.dart';
 import 'package:hotel_management_system/views/screens/screens.dart';
